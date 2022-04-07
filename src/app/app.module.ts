991060d6df805fac343555cc90a463437d77cbd0
Tweniee <abhisheku361@gmail.com>
2022-04-07 19:41:39 +0530

    All component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateToDoComponent } from './TodoList/create-to-do/create-to-do.component';
+import { ToDoListComponent } from './TodoList/create-to-do/PendingList/to-do-list/to-do-list.component';
+import { DoneListComponent } from './TodoList/create-to-do/CompletedList/done-list/done-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateToDoComponent,
+    ToDoListComponent,
+    DoneListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
